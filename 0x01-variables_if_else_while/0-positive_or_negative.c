08471f9e6dda9c2f903f993cb862e8f6dcb56aa0
Henok <hhenok124@gmail.com>
2023-02-18 00:00:26 -0800

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int n;
 srand(time(0));
 n = rand() -  RAND_MAX / 2;
 if (n > 0)
-printf("%dis positive", n);
+printf("is positive", n);
 else if  (n < 0)
-printf("%dis negative", n);
+printf("is negative", n);
 else
-printf("%dis zero", n);
+printf("is zero", n);
 return (0);
 }
