ddaa373a56cf6b464169a389915be42fbc1890e5
Henok <hhenok124@gmail.com>
2023-02-18 03:03:21 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - ENTRY POINT
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 'a';
+
+	while
+		(c >= 'z')
+	{
+		putchar(c);
+			c--;
+	}
+	putchar('\n');
+	return (0);
+}
