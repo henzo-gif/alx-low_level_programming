844604b50f5d16b572ec0e4ac8868edc5fbb9052
Henok <hhenok124@gmail.com>
2023-02-18 04:11:52 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,14 @@
 int main(void)
 {
 	int h;
-
-	for (h = 0; h < 10; h++)
-	{		
-		putchar(h + '0');
-	if (h < 9)
+	for (h = 0;  h < 10; h++)
 	{
-	putchar(',');
-	putchar(' ');
-	}
+		putchar(h + '0');
+		if (h < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
