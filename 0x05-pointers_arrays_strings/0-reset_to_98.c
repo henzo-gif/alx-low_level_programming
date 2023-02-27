b77d3737fc47892a0be2432ab80ce4a5c65eeea0
Henok <hhenok124@gmail.com>
2023-02-27 12:07:49 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - take a pointer ab int as parameter and uodates
+ * the value it points to to 98
+ * @n: point to an int
+ * Return: Always 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
