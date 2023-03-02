86583fa1086c0e4fe30a147db2bbcde281f96b30
Henok <hhenok124@gmail.com>
2023-03-02 05:41:05 -0800

    task-6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowecase to upper case
+ * @n: pointer
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int a;
+
+	a = 0;
+	while (n[a] != '\0')
+	{
+		if (n[a] >= 'a' && n[a] <= 'z')
+			n[a] = n[a] - 32;
+		a++;
+	}
+	return (n);
+}
