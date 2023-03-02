43eac1810674e60d1ff485348a80ea660ac835f6
Henok <hhenok124@gmail.com>
2023-03-02 05:27:20 -0800

    task-4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * _strcmp - compare string values
  * @s1: input
  * @s2: input
- * Return: s1[a] - s2[b]
+ * Return: s1[a] - s2[a]
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 	a = 0;
 	while (s1[a] != '\0' && s2[b] != '\0')
 	{
-		if (s1[a] != s2[b])
+		if (s1[a] != s2[a])
 		{
-			return (s1[a] - s2[b]);
+			return (s1[a] - s2[a]);
 		}
 		a++;
 	}
