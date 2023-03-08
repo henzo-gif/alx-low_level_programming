248f20a0034ba80060731b8ff4f4fc05121a93dd
Henok <hhenok124@gmail.com>
2023-03-08 03:18:51 -0800

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,3 +16,13 @@ int find_sqrt(int num, int root)
 		return (-1);
 	return (find_sqrt(num, root + 1));
 }
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (find_sqrt(n, root));
+}
