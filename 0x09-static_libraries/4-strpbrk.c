6474f7f5a0e5387d374cb5275b5e76abb94bb5b7
Henok <hhenok124@gmail.com>
2023-03-11 08:55:32 -0800

    0x09 static libraries

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+        int q;
+
+        while (*s)
+        {
+                for (q = 0; accept[q]; q++)
+                {
+                        if (*s == accept[q])
+                                return (s);
+                }
+                s++;
+        }
+        return ('\0');
+}
