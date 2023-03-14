e2be9c3b56215f1042c37af9843b41dc152a018d
Henok <hhenok124@gmail.com>
2023-03-14 01:38:32 -0700

    0-create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main -  a function that creates an array of chars, and initializes it with a specific char.
+ * Return: NULL if size = 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *c[] = "henok";
+	putchar("%s\n", c);
+	return (0);
+
+}
