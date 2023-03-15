9c59883526eb02d48a3b3770fefa51e794791391
Henok <hhenok124@gmail.com>
2023-03-15 00:46:14 -0700

    1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,22 +4,22 @@
 /**
  * _strdup - returns a pointer to a newly allocated space in memory
  * @str: char
- * Return: NUll if str = NULL
+ * Return: 0
  */
 char *_strdup(char *str)
 {
-	char *ss;
-	int i, c = 0;
+	char *aaa;
+	int i, r = 0;
 
 	if (str == NULL)
 		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	ss = malloc(sizeof(char) * (i + 1));
+	aaa = malloc(sizeof(char) * (i + 1));
 	if (ss == NULL)
 		return (NULL);
-	for (c = 0; str[c]; c++)
+	for (r = 0; str[r]; r++)
 		ss[c] = str[c];
 	return (ss);
 }
