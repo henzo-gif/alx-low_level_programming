4a66e5d20a0f8ec21da899ee2183b1c48bce4c09
Henok <hhenok124@gmail.com>
2023-03-16 06:05:32 -0700

    1-sring

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (!S)
+	if (*S = 0)
 		return (NULL);
 	while (i < len1)
 	{
